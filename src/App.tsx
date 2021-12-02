9ced5c2093d327ade05975b11e009216598c65fe
Palina.Ulashchyk <palina.ulashchyk@itechart-group.com>
2021-12-02 19:00:40 +0300

    add: props typing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import UserList from "./components/UserList";
+import Card, { CardVariants } from "./components/Card";
 
 
 const App = () => {
   return (
     <div>
       <UserList />
-
+      <Card width="200px" height="200px" variant={CardVariants.outlined}>
+        <button>Button</button>
+      </Card>
     </div>
   );
 };
---
+++ src/components/Card.tsx
@@ -0,0 +1,25 @@
+import React, {FC} from 'react';
+
+export enum CardVariants {
+    outlined = "outlined",
+    primary = "primary"
+}
+
+interface CardProps {
+    width?: string;
+    height?: string;
+    variant?: CardVariants;
+}
+
+const Card: FC<CardProps>= ({ width, height, variant, children }) => {
+    return (
+        <div style={{ 
+            width, height, 
+            background: variant === CardVariants.primary ? "purple": "none" ,
+            border: variant === CardVariants.outlined ? "4px solid purple": "none" }}>
+            {children}
+        </div>
+    );
+};
+
+export default Card;
