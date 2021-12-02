140fa5c19c8a9ba23804f0073c2d0c3634461b98
Palina.Ulashchyk <palina.ulashchyk@itechart-group.com>
2021-12-02 18:00:57 +0300

    add: mapping data

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import { useSelector } from "react-redux";
+import React, { useEffect } from 'react';
+import { useDispatch } from "react-redux";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { fetchUsers } from "../store/action-creators/user";
 
 const UserList: React.FC = () => {
-    const state = useSelector(state => state)
+    const {error, users, loading} = useTypedSelector(state => state.user)
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsers())   
+    }, [])
+
+    if(loading) {
+        return <h2>Loading...</h2>
+    }
+
+    if(error){
+      return <h2>{error}</h2>
+    }
 
     return (
         <div>
-            
+            {users.map((user) => {
+                return <h4 key={user.id}>{user.name}</h4>
+            })}
         </div>
     );
 };
---
+++ src/store/action-creators/user.ts
@@ -0,0 +1,24 @@
+import { Dispatch } from "react";
+import axios from "axios";
+
+import { UserAction, UserActionTypes } from "../../types/todo";
+
+export const fetchUsers = () => {
+  return async (dispatch: Dispatch<UserAction>) => {
+    try {
+      dispatch({ type: UserActionTypes.FETCH_USERS });
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      dispatch({
+        type: UserActionTypes.FETCH_USERS_SUCCESS,
+        payload: response.data,
+      });
+    } catch (e) {
+      dispatch({
+        type: UserActionTypes.FETCH_USERS_ERROR,
+        payload: "Users loading error",
+      });
+    }
+  };
+};
